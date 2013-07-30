16e5e32731cbb29db2906482a0e6d8d1ffcaf5ce
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-07-29 23:21:44 -0700

    I wrote quick sort

---
+++ QuickSort/QuickSort/QuickSort.fs
@@ -0,0 +1,8 @@
+﻿namespace AndrewGaspar
+
+module QuickSort = 
+    let rec quick list =
+        match list with 
+        | ([] | [_]) -> list
+        | head::tail -> let lesser, greater = List.partition((>=) head) tail
+                        quick lesser @ [head] @ quick greater
